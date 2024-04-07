cfdcb8389e0ab1ca543d2a6b1879089d2ccfa836
zelong <zelong.guo@outlook.com>
2024-04-07 15:55:38 +0200

    update test.py

---
+++ test.py
@@ -0,0 +1,27 @@
+import dislocation as dl 
+import numpy as np 
+
+mu = 3e10
+nu = 0.25
+
+#model = np.array([
+#    440               ,3940             ,15,80,50,50, 45,    1,    1, 0, 
+#	])
+
+model = np.array([
+    [440.58095043254673,3940.114839963042,15,80,50,50, 45, 0.01, 0.01, 0], 
+	[440.58095043254673,3940.114839963042,15,80,50,50, 45, 0.01, 0.01, 0]])
+# print(f"model: {model}")
+
+obs = np.array([454, 3943, 0])
+#obs = np.array([454, 3943, 0])
+#print(f"obs: {obs}")
+
+# length, width, depth, dip, strike, easting, northing, str-slip, dip-selip. opening
+
+# TODO: single stations (or single fault patch) still has problem to be passed to C API. Still need test!
+
+dl.okada_rect(obs, model, mu, nu)
+
+
+
