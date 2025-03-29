88a62eae9d248bfaeb8e3d397d07d410a6c17a75
zelong <zelong.guo@outlook.com>
2025-03-29 14:04:06 +0100

    use find_package looking for python-related directory

--- cmake/dislocations.cmake
+++ cmake/dislocations.cmake
@@ -2,38 +2,59 @@
 # Zelong Guo, @ Potsdam, DE
 
 # ------------------------ Dislocations ------------------------
-# -------- Python related stuff  --------
-# Allow user to specify the Python environment path:
-# cmake .. -DPYTHON_ENV_PATH=/path/to/your/env
-if(NOT DEFINED PYTHON_ENV_PATH)
-    set(PYTHON_ENV_PATH "/Users/zelong/opt/miniconda3/envs/temp") # My default path
+# Activate the vitual environment, then CMake would find the python and numpy in this env, necessary for Mac
+find_program(Python3_EXECUTABLE python REQUIRED)
+
+# Find Python and NumPy in current environment
+find_package(Python3 COMPONENTS Interpreter Development NumPy REQUIRED)
+if(NOT Python3_FOUND)
+    message(FATAL_ERROR "Python3 not found")
 endif()
-message(STATUS "Using Python environment: ${PYTHON_ENV_PATH}")
-
-# Auto-detect Python include directory
-execute_process(
-    COMMAND "${PYTHON_ENV_PATH}/bin/python" -c "import sysconfig; print(sysconfig.get_path('include'))"
-    OUTPUT_VARIABLE PYTHON_INCLUDE_DIR
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-message(STATUS "Python include dir: ${PYTHON_INCLUDE_DIR}")
-
-# Auto-detect NumPy include directory
-execute_process(
-    COMMAND "${PYTHON_ENV_PATH}/bin/python" -c "import numpy; print(numpy.get_include())"
-    OUTPUT_VARIABLE NUMPY_INCLUDE_DIR
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    RESULT_VARIABLE NUMPY_STATUS
-)
-# If numpy not found
-if(NOT NUMPY_STATUS EQUAL 0)
-    message(FATAL_ERROR "NumPy not found in the specified Python environment!")
+if(NOT Python3_NumPy_FOUND)
+    message(FATAL_ERROR "NumPy not found")
 endif()
-message(STATUS "NumPy include dir: ${NUMPY_INCLUDE_DIR}")
 
-# Auto-detect Python site-packages directory
+message(STATUS "Python include dir: ${Python3_INCLUDE_DIRS}")
+message(STATUS "NumPy include dir: ${Python3_NumPy_INCLUDE_DIRS}")
+
+# -------- Source Files --------
+aux_source_directory (${CMAKE_SOURCE_DIR}/src/core/ SOURCES)
+
+# -------- Objectives --------
+# Pythin C Extension Module:
+add_library(dislocations SHARED)
+target_sources(dislocations PRIVATE ${CMAKE_SOURCE_DIR}/src/bindings/dislocations.c ${SOURCES})
+
+# Including header files of Python and Numpy:
+target_include_directories(dislocations PRIVATE
+    ${CMAKE_SOURCE_DIR}/include/
+    ${Python3_INCLUDE_DIRS}
+    ${Python3_NumPy_INCLUDE_DIRS}
+)
+
+# ------ Build Options -------
+# Complier Options, if you want debugging:
+target_compile_options(dislocations PRIVATE
+    -O2
+    -fPIC
+)
+
+# Linking Options go here:
+if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+    set(CMAKE_SHARED_LINKER_FLAGS "-undefined dynamic_lookup")
+endif()
+if(NOT CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+    target_link_libraries(dislocations PRIVATE ${Python3_LIBRARIES})
+endif()
+
+set_target_properties(dislocations PROPERTIES
+    PREFIX ""
+    SUFFIX ".so"
+)
+
+# Get Python site-packages directory
 execute_process(
-    COMMAND "${PYTHON_ENV_PATH}/bin/python" -c 
+    COMMAND "python" -c
         "import sys, site; print(site.getsitepackages()[0] if hasattr(site, 'getsitepackages') else sys.prefix)"
     OUTPUT_VARIABLE PYTHON_SITE_PACKAGES
     OUTPUT_STRIP_TRAILING_WHITESPACE
@@ -45,48 +66,6 @@ if(NOT EXISTS ${PYTHON_SITE_PACKAGES})
 endif()
 message(STATUS "Installation path: ${PYTHON_SITE_PACKAGES}")
 
-# -------- Source Files --------
-# set(SOURCES
-#     ${CMAKE_SOURCE_DIR}/src/core/okada_dc3d.c
-#     ${CMAKE_SOURCE_DIR}/src/core/okada_disloc3d.c
-#     # ${CMAKE_SOURCE_DIR}/src/core/okada_dc3d0.c
-#     # ${CMAKE_SOURCE_DIR}/src/core/okada_disloc3d0.c
-# )
-aux_source_directory (${CMAKE_SOURCE_DIR}/src/core/ SOURCES)
-
-# -------- Objectives --------
-# Pythin C Extension Module:
-add_library(dislocations SHARED)
-target_sources(dislocations PRIVATE ${CMAKE_SOURCE_DIR}/src/bindings/dislocations.c ${SOURCES})
-
-# Including header files of Python and Numpy:
-target_include_directories(dislocations PRIVATE
-    ${CMAKE_SOURCE_DIR}/include/
-    ${PYTHON_INCLUDE_DIR}
-    ${NUMPY_INCLUDE_DIR}
-)
-
-# ------ Build Options -------
-# Complier Options, if you want debugging:
-target_compile_options(dislocations PRIVATE
-    -O2
-    -fPIC
-)
-# Linking Options go here:
-if (CMAKE_SYSTEM_NAME STREQUAL "Darwin")  # for macOS
-    set(CMAKE_SHARED_LINKER_FLAGS "-undefined dynamic_lookup")
-elseif (CMAKE_SYSTEM_NAME STREQUAL "Linux")
-    set(CMAKE_SHARED_LINKER_FLAGS "-Wl,-rpath,/usr/local/lib")
-elseif (CMAKE_SYSTEM_NAME STREQUAL "Windows")
-    set(CMAKE_SHARED_LINKER_FLAGS "-Wl,--export-all-symbols -Wl,--enable-auto-import")
-endif()
-
-set_target_properties(dislocations PROPERTIES
-    SUFFIX ".so"
-    PREFIX ""
-)
-
-# Install the library to the Python site-packages:
 install(
     TARGETS dislocations
     LIBRARY
